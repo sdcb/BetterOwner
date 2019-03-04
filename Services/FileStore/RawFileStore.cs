b41afad9cc576e9e2dee14daae21fdfcaaf480db
sdcb <sdflysha@qq.com>
2019-03-04 17:53:22 +0800

    stage - in progress.

---
+++ Services/FileStore/RawFileStore.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.IO;
+
+namespace BetterOwner.Services.FileStore
+{
+    public class RawFileStore : IFileStore
+    {
+        private readonly IDbConnection _db;
+
+        public RawFileStore(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public Stream Download(string collection, Guid fileId)
+        {
+            return new MemoryStream(_db.QueryFirst<byte[]>($"SELECT Stream FROM [{collection}] WHERE Id = '{fileId}'"));
+        }
+
+        public Guid Upload(string collection, Stream content)
+        {
+            _db.Query<Guid>($"INSERT INTO [{collection}] (Content) VALUES @")
+        }
+    }
+}
