ecbfea5f3e6ff6de18b4e706eccaac60d34c0cc6
sdcb <sdflysha@qq.com>
2019-03-12 15:14:39 +0800

    add missing publish.api.ts

---
+++ ClientApp/src/app/pages/publish/publish.api.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class PublishApiService {
+  constructor(private httpClient: HttpClient) { }
+
+  create(title: string, price: number, description: string, files: File[]) {
+    const form = new FormData();
+    form.append('title', title);
+    form.append('price', price.toString());
+    form.append('description', description);
+    for (const file of files) { form.append('files', file, file.name); }
+    return this.httpClient.post('/publish/create', form);
+  }
+}
